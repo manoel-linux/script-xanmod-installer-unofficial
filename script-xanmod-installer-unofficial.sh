108d34339d49b069788a69c39b0a5aa9dd52ada1
manoel-linux <135872910+manoel-linux@users.noreply.github.com>
2023-07-31 23:08:02 -0300

    Update script-xanmod-installer-unofficial.sh

--- script-xanmod-installer-unofficial.sh
+++ script-xanmod-installer-unofficial.sh
@@ -235,6 +235,7 @@ echo "#################################################################"
 echo "Installation completed."
 echo "#################################################################"
 echo "To update Xanmod, use the following command: sudo apt-get update && sudo apt-get upgrade"
+echo "#################################################################"
 echo "(Attention!) >> You need to execute the script upon restarting the machine to remove the old kernel without conflicts. 
 You must first check if Ubuntu/Debian is running the Xanmod kernel. 
 Then, proceed to remove all other kernels and keep only the Xanmod. Give remove-kernel.sh executable permissions using chmod a+x remove-kernel.sh, 
